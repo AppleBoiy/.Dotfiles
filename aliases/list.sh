09ca993ad50e9321123ef4ed2c60df395a80b9e8
ICCIE <115919485+AppleBoiy@users.noreply.github.com>
2023-01-25 13:47:18 +0700

    initial commit

---
+++ aliases/list.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+  # git
+  alias gs="git status"
+
+  # map exa commands to normal ls commands
+  alias ll="exa -l -g --icons"
+  alias ls="exa --icons"
+  alias lt="exa --tree --icons -a -I '.git|__pycache__|.mypy_cache|.ipynb_checkpoints'"
+
+  # starship prompt
+  alias gt="starship toggle gcloud disabled" # toggle gcloud info on the prompt
+  
